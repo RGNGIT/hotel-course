bf63230f74e340ec479711578efc7f0cea96ac74
RGNGIT <vnifedow@bk.ru>
2023-09-19 01:27:10 +0500

    Some updates

--- Hotel-Course/App.Designer.cs
+++ Hotel-Course/App.Designer.cs
@@ -30,27 +30,33 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            buttonAddRequest = new Button();
             tabControl2 = new TabControl();
             tabPage3 = new TabPage();
             labelDescriptionInstant = new Label();
             label5 = new Label();
             comboBoxInstantNumber = new ComboBox();
-            label4 = new Label();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            label2 = new Label();
-            dateTimePicker2 = new DateTimePicker();
-            dateTimePicker1 = new DateTimePicker();
             tabPage4 = new TabPage();
+            dataGridViewNumbers = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            textBoxRequestHolder = new TextBox();
+            dateTimePickerRequestFrom = new DateTimePicker();
+            dateTimePickerRequestTo = new DateTimePicker();
+            label4 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             tabPage2 = new TabPage();
             buttonAddDay = new Button();
             label1 = new Label();
             dateTimePickerCurrentDate = new DateTimePicker();
-            buttonAddRequest = new Button();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             tabControl2.SuspendLayout();
             tabPage3.SuspendLayout();
+            tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewNumbers).BeginInit();
             tabPage2.SuspendLayout();
             SuspendLayout();
             // 
@@ -66,7 +72,14 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(buttonAddRequest);
             tabPage1.Controls.Add(tabControl2);
+            tabPage1.Controls.Add(textBoxRequestHolder);
+            tabPage1.Controls.Add(dateTimePickerRequestFrom);
+            tabPage1.Controls.Add(dateTimePickerRequestTo);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label3);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
@@ -75,32 +88,36 @@
             tabPage1.Text = "Заявка";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // buttonAddRequest
+            // 
+            buttonAddRequest.Location = new Point(6, 385);
+            buttonAddRequest.Name = "buttonAddRequest";
+            buttonAddRequest.Size = new Size(200, 23);
+            buttonAddRequest.TabIndex = 9;
+            buttonAddRequest.Text = "Оставить заявку";
+            buttonAddRequest.UseVisualStyleBackColor = true;
+            buttonAddRequest.Click += buttonAddRequest_Click;
+            // 
             // tabControl2
             // 
             tabControl2.Controls.Add(tabPage3);
             tabControl2.Controls.Add(tabPage4);
-            tabControl2.Location = new Point(3, 3);
+            tabControl2.Location = new Point(212, 3);
             tabControl2.Name = "tabControl2";
             tabControl2.SelectedIndex = 0;
-            tabControl2.Size = new Size(783, 412);
+            tabControl2.Size = new Size(574, 409);
             tabControl2.TabIndex = 0;
+            tabControl2.TabIndexChanged += tabControl2_TabIndexChanged;
             // 
             // tabPage3
             // 
-            tabPage3.Controls.Add(buttonAddRequest);
             tabPage3.Controls.Add(labelDescriptionInstant);
             tabPage3.Controls.Add(label5);
             tabPage3.Controls.Add(comboBoxInstantNumber);
-            tabPage3.Controls.Add(label4);
-            tabPage3.Controls.Add(label3);
-            tabPage3.Controls.Add(textBox1);
-            tabPage3.Controls.Add(label2);
-            tabPage3.Controls.Add(dateTimePicker2);
-            tabPage3.Controls.Add(dateTimePicker1);
             tabPage3.Location = new Point(4, 24);
             tabPage3.Name = "tabPage3";
             tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(775, 384);
+            tabPage3.Size = new Size(566, 381);
             tabPage3.TabIndex = 0;
             tabPage3.Text = "Заселиться в текущий момент";
             tabPage3.UseVisualStyleBackColor = true;
@@ -116,7 +133,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(212, 24);
+            label5.Location = new Point(6, 3);
             label5.Name = "label5";
             label5.Size = new Size(45, 15);
             label5.TabIndex = 7;
@@ -125,69 +142,95 @@
             // comboBoxInstantNumber
             // 
             comboBoxInstantNumber.FormattingEnabled = true;
-            comboBoxInstantNumber.Location = new Point(263, 21);
+            comboBoxInstantNumber.Location = new Point(6, 21);
             comboBoxInstantNumber.Name = "comboBoxInstantNumber";
-            comboBoxInstantNumber.Size = new Size(506, 23);
+            comboBoxInstantNumber.Size = new Size(554, 23);
             comboBoxInstantNumber.TabIndex = 6;
             comboBoxInstantNumber.SelectedValueChanged += comboBox1_SelectedValueChanged;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(dataGridViewNumbers);
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(566, 381);
+            tabPage4.TabIndex = 1;
+            tabPage4.Text = "Забронировать номера на определенный срок";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewNumbers
+            // 
+            dataGridViewNumbers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewNumbers.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridViewNumbers.Location = new Point(6, 6);
+            dataGridViewNumbers.Name = "dataGridViewNumbers";
+            dataGridViewNumbers.RowTemplate.Height = 25;
+            dataGridViewNumbers.Size = new Size(554, 369);
+            dataGridViewNumbers.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Номер";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Стоимость";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Доп. инфо.";
+            Column3.Name = "Column3";
+            // 
+            // textBoxRequestHolder
+            // 
+            textBoxRequestHolder.Location = new Point(6, 27);
+            textBoxRequestHolder.Name = "textBoxRequestHolder";
+            textBoxRequestHolder.Size = new Size(200, 23);
+            textBoxRequestHolder.TabIndex = 3;
+            // 
+            // dateTimePickerRequestFrom
+            // 
+            dateTimePickerRequestFrom.Location = new Point(6, 71);
+            dateTimePickerRequestFrom.Name = "dateTimePickerRequestFrom";
+            dateTimePickerRequestFrom.Size = new Size(200, 23);
+            dateTimePickerRequestFrom.TabIndex = 0;
+            // 
+            // dateTimePickerRequestTo
+            // 
+            dateTimePickerRequestTo.Location = new Point(6, 115);
+            dateTimePickerRequestTo.Name = "dateTimePickerRequestTo";
+            dateTimePickerRequestTo.Size = new Size(200, 23);
+            dateTimePickerRequestTo.TabIndex = 1;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 91);
+            label4.Location = new Point(6, 97);
             label4.Name = "label4";
             label4.Size = new Size(73, 15);
             label4.TabIndex = 5;
             label4.Text = "Дата выезда";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(3, 47);
-            label3.Name = "label3";
-            label3.Size = new Size(69, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Дата заезда";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(6, 21);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 23);
-            textBox1.TabIndex = 3;
-            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(3, 3);
+            label2.Location = new Point(3, 9);
             label2.Name = "label2";
             label2.Size = new Size(67, 15);
             label2.TabIndex = 2;
             label2.Text = "Держатель";
             // 
-            // dateTimePicker2
+            // label3
             // 
-            dateTimePicker2.Location = new Point(6, 109);
-            dateTimePicker2.Name = "dateTimePicker2";
-            dateTimePicker2.Size = new Size(200, 23);
-            dateTimePicker2.TabIndex = 1;
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(6, 65);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
-            dateTimePicker1.TabIndex = 0;
-            // 
-            // tabPage4
-            // 
-            tabPage4.Location = new Point(4, 24);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(775, 384);
-            tabPage4.TabIndex = 1;
-            tabPage4.Text = "Забронировать номера на определенный срок";
-            tabPage4.UseVisualStyleBackColor = true;
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Дата заезда";
             // 
             // tabPage2
             // 
@@ -228,16 +271,6 @@
             dateTimePickerCurrentDate.Size = new Size(162, 23);
             dateTimePickerCurrentDate.TabIndex = 0;
             // 
-            // buttonAddRequest
-            // 
-            buttonAddRequest.Location = new Point(595, 355);
-            buttonAddRequest.Name = "buttonAddRequest";
-            buttonAddRequest.Size = new Size(174, 23);
-            buttonAddRequest.TabIndex = 9;
-            buttonAddRequest.Text = "Оставить заявку";
-            buttonAddRequest.UseVisualStyleBackColor = true;
-            buttonAddRequest.Click += buttonAddRequest_Click;
-            // 
             // App
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -248,9 +281,12 @@
             Text = "Form1";
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             tabControl2.ResumeLayout(false);
             tabPage3.ResumeLayout(false);
             tabPage3.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewNumbers).EndInit();
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
             ResumeLayout(false);
@@ -271,11 +307,15 @@
         private ComboBox comboBoxInstantNumber;
         private Label label4;
         private Label label3;
-        private TextBox textBox1;
+        private TextBox textBoxRequestHolder;
         private Label label2;
-        private DateTimePicker dateTimePicker2;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePickerRequestTo;
+        private DateTimePicker dateTimePickerRequestFrom;
         private Label labelDescriptionInstant;
         private Button buttonAddRequest;
+        private DataGridView dataGridViewNumbers;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
     }
 }
--- Hotel-Course/App.cs
+++ Hotel-Course/App.cs
@@ -8,43 +8,56 @@ namespace Hotel_Course
         {
             InitializeComponent();
             FillSelectComboInstant();
+            FillSelectNumbersGrid();
+        }
+
+        Dictionary<string, string> defineType = new Dictionary<string, string>()
+        {
+            { "Luxe", "Люкс" },
+            { "SemiLuxe", "Полулюкс" },
+            { "SingleRoom", "Однокомнатный" },
+            { "DoubleRoom", "Двукомнатный" },
+            { "RoomWithSofa", "Двукомнатный с диванами" }
+        };
+
+        string DefineNumberDetails(Models.Number number)
+        {
+            string typeOf = defineType[number.GetType().Name];
+            string temp = $"Номер {typeOf}. Стоимость {number.Cost}р. ";
+
+            switch (typeOf)
+            {
+                case "Люкс":
+                    return temp + $"Количество ванных комнат: {((Models.Luxe)number).AmountOfBathrooms}";
+
+                case "Полулюкс":
+                    return temp + $"Дополнительно: {((Models.SemiLuxe)number).SemiLuxeAddings}";
+
+                case "Однокомнатный":
+                    return temp + $"Количество кроватей: {((Models.SingleRoom)number).AmountOfBeds}";
+
+                case "Двукомнатный":
+                    return temp + $"Количество кроватей: {((Models.DoubleRoom)number).AmountOfBeds}";
+
+                case "Двукомнатный с диванами":
+                    return temp + $"Количество диванов: {((Models.RoomWithSofa)number).NumberOfSofas}";
+            }
+
+            return "Невозможно определить";
+        }
+
+        void FillSelectNumbersGrid()
+        {
+            dataGridViewNumbers.Rows.Clear();
+
+            Storage.Numbers.ForEach(n => dataGridViewNumbers.Rows.Add(defineType[n.GetType().Name], n.Cost, DefineNumberDetails(n)));
         }
 
         void FillSelectComboInstant()
         {
-            string numberItem(Models.Number number)
-            {
-                Dictionary<string, string> defineType = new Dictionary<string, string>()
-                {
-                    { "Luxe", "Люкс" },
-                    { "SemiLuxe", "Полулюкс" },
-                    { "SingleRoom", "Однокомнатный" },
-                    { "DoubleRoom", "Двукомнатный" },
-                    { "RoomWithSofa", "Двукомнатный с диванами" }
-                };
-
-                string typeOf = defineType[number.GetType().Name];
-                string temp = $"Номер {typeOf}. Стоимость {number.Cost}р. ";
-
-                switch (typeOf)
-                {
-                    case "Люкс":
-                        return temp + $"Количество ванных комнат: {((Models.Luxe)number).AmountOfBathrooms}";
-                    case "Полулюкс":
-                        return temp + $"Дополнительно: {((Models.SemiLuxe)number).SemiLuxeAddings}";
-                    case "Однокомнатный":
-                        return temp + $"Количество кроватей: {((Models.SingleRoom)number).AmountOfBeds}";
-                    case "Двукомнатный":
-                        return temp + $"Количество кроватей: {((Models.DoubleRoom)number).AmountOfBeds}";
-                    case "Двукомнатный с диванами":
-                        return temp + $"Количество диванов: {((Models.RoomWithSofa)number).NumberOfSofas}";
-                }
-
-                return "Невозможно определить";
-            }
-
             comboBoxInstantNumber.Items.Clear();
-            Storage.Numbers.ForEach(n => comboBoxInstantNumber.Items.Add(numberItem(n)));
+
+            Storage.Numbers.ForEach(n => comboBoxInstantNumber.Items.Add(DefineNumberDetails(n)));
         }
 
         private void buttonAddDay_Click(object sender, EventArgs e)
@@ -60,6 +73,12 @@ namespace Hotel_Course
         private void buttonAddRequest_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void tabControl2_TabIndexChanged(object sender, EventArgs e)
+        {
+            FillSelectComboInstant();
+            FillSelectNumbersGrid();
         }
     }
 }
